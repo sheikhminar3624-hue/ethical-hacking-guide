38cc766afd27c00dbef76e0e0419756bc29eccf3
sheikhminar3624-hue <sheikhminar3624@gmail.com>
2026-03-17 13:30:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+// Simple content filter system
+
+const bannedWords = [
+  "sex",
+  "porn",
+  "xxx",
+  "adult",
+  "nude",
+  "18+"
+];
+
+function filterContent() {
+  const bodyText = document.body.innerHTML.toLowerCase();
+
+  for (let word of bannedWords) {
+    if (bodyText.includes(word)) {
+      document.body.innerHTML = `
+        <div style="text-align:center;padding:100px;">
+          <h1 style="color:red;">Content Blocked</h1>
+          <p>This page contains restricted content.</p>
+        </div>
+      `;
+      break;
+    }
+  }
+}
+
+filterContent();
